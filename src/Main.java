8a2334565739993b9e83d1fa23cfb258f363ee86
FelicsXIII <felics1689@gmail.com>
2023-11-14 12:09:33 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double height = 1.75;
+        int weight = 57;
+        int bmi = service.calculate(height,weight);
+        System.out.println("Индекс массы тела: " + bmi);
+    }
+}
